4614659befee5982869cf79b53db11adffae9468
DESKTOP-8EFFHO4\seita_v <seita.1224.ds@outlook.com>
2016-10-21 02:02:49 +0900

    作成クラス（前回コミット時に追加した）のインポート文追加

--- ChooMe/app/src/main/java/com/example/itzemi/choome/MainActivity.java
+++ ChooMe/app/src/main/java/com/example/itzemi/choome/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.itzemi.choome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.itzemi.DBController.Goodsdata;
+import com.example.itzemi.DBController.Reviewdata;
+import com.example.itzemi.DBController.Userdata;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
